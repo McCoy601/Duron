700d2e2bcecf7368ee979cf8bd0896b73752e9da
汪少同学 <wangshao429@qq.com>
2017-11-16 21:09:29 +0800

    将日历部分抽取出来成为日历模板

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,9 +2,16 @@
 
 Page({
   data: {
-    calendar:{
-      taboo:false//宜忌
-    },
-    motto:'如果努力有用，还要天赋干嘛！'
+    obj1: {
+      era: '丙申猴年 辛丑月 庚子日',
+      lunar: '腊月十六',
+      week: '星期五',
+      year:2017,
+      mouth:11,
+      day:16,
+      act: '混吃等死',
+      taboo: false,//宜忌
+      motto: '如果努力有用，还要天赋干嘛！'
+    }
   }
 })
